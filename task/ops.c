3912d4165ffcd600c18c017d9935d739dba232d3
Sanghyun Hong <sanghyun.hong@oregonstate.edu>
2025-04-30 05:40:01 -0700

    Hotfix

--- task/ops.c
+++ task/ops.c
@@ -1,15 +1,83 @@
-#ifndef __OPS_H_DEFINED__
-#define __OPS_H_DEFINED__
-
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 /*
  *  A function that does the matrix multiplication.
+ *   - X: 1   x 784 (input)
+ *   - W: 128 x 784 (weights)
+ *   - O: 1   x 128 (output)
  */
 void mul2D(
   float** result,
   float* x, size_t xh, size_t xw, \
-  float* w, size_t wh, size_t ww);
+  float* w, size_t wh, size_t ww)
+{
 
-#endif
+  /*
+   * Error cases: in multiplication of X * W...
+   */
+  if (xw != ww) {
+    printf("[mul2D] Error: dimensions should match " \
+           "[x-width: %zd != w-width: %zd]\n", xw, ww);
+    exit(1);
+  }
+
+  /*
+   *  Dimensions of resulting matrices
+   *  (Note: W [weight matrix] is already transposed, so consider different edge-lengths)
+   */
+  size_t res_size  = xh * wh;
+
+  /*
+   *  Memory allocations for the results
+   */
+  float* mresult = (float *) malloc(res_size * sizeof(float));
+  if (mresult == NULL) {
+    printf("[mul2D] Error: cannot allocate memory for the result\n");
+    exit(1);
+  }
+
+  /*
+   *  Do the multiplication:
+   *  Note that this code block comes from the ATen GEMM in PyTorch
+   */
+  size_t i, j, k;
+
+  float *w_ = w;
+  for (i = 0; i < xh; i++)
+  {
+    float *x_ = x;
+    for (j = 0; j < wh; j++)
+    {
+
+      /*
+       *  Multiplication for a single element
+       *   in the resulting matrix (i-x-j = ij)
+       */
+      float sum = 0.;
+      for (k = 0; k < ww /* same as xw */; k++) {
+
+        // multiplications
+        sum += x_[k] * w_[k];
+
+      }
+
+      /*
+       *  store the multiplication result,
+       *   and increase the pointer by 'ww'
+       */
+      mresult[i*wh + j] = sum;
+      w_ += ww;
+
+    }
+    x_ += xw;
+
+  }
+
+  /*
+   *  Store and pass the pointers to the caller
+   */
+  *result = mresult;
+}
